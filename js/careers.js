9f331a75160795bf9286afe4ae9f0bc42bee826e
KIRUBHAKARAN <kirubhag@gmail.com>
2021-02-21 22:23:12 +0530

    Tips content updated.
    Tips content updated.

--- js/careers.js
+++ js/careers.js
@@ -1,8 +1,21 @@
 /* 
-    Created on : 13 Feb, 2021, 3:49:32 PM
-    Author     : kirubha-2911
-*/
+ Created on : 13 Feb, 2021, 3:49:32 PM
+ Author     : kirubha-2911
+ */
 
-$(document).ready(function(){
-    
+var h = document.documentElement,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight',
+        progress = document.querySelector('.progress'),
+        scroll;
+
+document.addEventListener('scroll', function () {
+    console.log("scroll");
+    scroll = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+    progress.style.setProperty('--scroll', scroll + '%');
+});
+
+$(document).ready(function () {
+
 });
